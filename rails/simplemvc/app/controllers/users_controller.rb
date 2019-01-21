316a386402d7ccd6670d5acebefdf901501fd405
Hani <inahsanjuan@gmail.com>
2019-01-21 21:15:46 +0800

    Added simplemvc

---
+++ rails/simplemvc/app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+        #render plain: @users.inspect
+    end
+
+    def new
+    end
+
+    def create
+        #params
+        User.create(
+            name: params[:user][:name],
+            email: params[:user][:email]
+        )
+
+        redirect_to "/"
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit
+        #/users/:id/edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        @user.update(
+            name: params[:user][:name],
+            email: params[:user][:email]
+        )
+
+        redirect_to "/"
+    end
+end
---
+++ rails/simplemvc/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "users#index"
+  get "/users/new", to: "users#new"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/edit", to: "users#edit"
+  patch "/users/:id", to: "users#update"
+end
