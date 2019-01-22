2afef294e1d5d406542daea67da912d279b951d0
Hani <inahsanjuan@gmail.com>
2019-01-22 15:13:30 +0800

    Added delete function

--- rails/simplemvc/app/controllers/users_controller.rb
+++ rails/simplemvc/app/controllers/users_controller.rb
@@ -27,12 +27,19 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.find(params[:id])
+        user = User.find(params[:id])
 
-        @user.update(
+        user.update(
             name: params[:user][:name],
             email: params[:user][:email]
         )
+
+        redirect_to "/"
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
 
         redirect_to "/"
     end
