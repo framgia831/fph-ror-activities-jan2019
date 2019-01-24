aed616811842e4d4a6a0d36f9c61853a5bb108d3
Hani <inahsanjuan@gmail.com>
2019-01-24 15:53:18 +0800

    Add create function with simple validation

--- rails/awesome_blog/app/controllers/users_controller.rb
+++ rails/awesome_blog/app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      params.require(:user).permit(:name, :email)
+    )
+
+    if @user.save
+      redirect_to root_url
+    else
+      render "new" #new.html.erb
+    end
   end
 end
